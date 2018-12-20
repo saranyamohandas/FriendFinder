7a85cdcfacb2bffcaecee767b5533fa17d208fc4
saranyamohandas <saranyamohandas2@gmail.com>
2018-12-20 14:32:41 -0800

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,17 @@
 var friendList = [{
     name: "Moksh",
-    ques1 : 4,
-    ques2 : 3,
-    ques3 : 4,
-    ques4 : 3,
-    ques5 : 4,
-    ques6 : 3,
-    ques7 : 4,
-    ques8 : 3,
-    ques9 : 4,
-    ques10 : 3
+    photo : "https://www.google.com/search?q=m+images&rlz=1C1JZAP_enUS702US702&tbm=isch&tbo=u&source=univ&sa=X&ved=2ahUKEwjQjdzv2K3fAhUL3lQKHU57C8sQsAR6BAgAEAE&biw=1536&bih=754&dpr=1.25#imgrc=GFcoUkc5jtT46M",
+    scores : [5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+             ]
 }
 ];
 module.exports = friendList;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,15 +5,37 @@ module.exports = function(app){
     app.get("/api/friends",function(req,res){
         res.json(friendData)
     });
-    app.post("/survey", function(req, res) {
+    app.post("/api/friends", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
     //if (tableData.length < 5) {
+      console.log("in post")
+      findMatch(req.body);
       friendData.push(req.body);
+      
       res.json(true);
+      //console.log(friendData);
     //}
     
   })
+};
 
+function findMatch(userInp){
+    //var diff = [];
+    var diff = 0; // calculate total of difference b/w scores
+    var matchCheck = [];
+    console.log("userinp - ",userInp)
+    console.log("friend data - ",friendData)
+    for(i=0;i<friendData.length;i++){
+        for(j=0;j<friendData[i].scores.length;j++){
+            diff += Math.abs(friendData[i].scores[j]) - userInp.scores[j];
+        }
+        matchCheck.push(diff);
+    }
+    matchCheck.sort(function(a,b){
+        return (a-b);
+    })
+    return(matchCheck[0])
 }
+

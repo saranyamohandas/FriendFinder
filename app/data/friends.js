825a40d668c1c44e2d87c9fbc62a9532152fe573
saranyamohandas <saranyamohandas2@gmail.com>
2018-12-20 20:26:33 -0800

    add pics

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendList = [
     {
-    name: "Moksh",
-    photo : "https://www.google.com/search?q=m+images&rlz=1C1JZAP_enUS702US702&tbm=isch&tbo=u&source=univ&sa=X&ved=2ahUKEwjQjdzv2K3fAhUL3lQKHU57C8sQsAR6BAgAEAE&biw=1536&bih=754&dpr=1.25#imgrc=GFcoUkc5jtT46M",
+    name: "Moksha",
+    photo : "assests/images/lovely-girl-with-curl.jpg",
     scores : [5,
       1,
       4,
@@ -16,7 +16,7 @@ var friendList = [
 },
     {
     name: "Bo",
-    photo : "https://www.google.com/search?q=dog+images&client=firefox-b-1-ab&tbm=isch&tbo=u&source=univ&sa=X&ved=2ahUKEwiKtu39-K_fAhVDKH0KHTE7B5sQsAR6BAgDEAE&biw=1536&bih=750#imgrc=OjyG2p3XfwpfOM",
+    photo : "assests/images/girl2.jpg",
     scores : [5,
       1,
       4,
@@ -30,8 +30,8 @@ var friendList = [
              ]
 },
     {
-    name: "Jack",
-    photo : "https://www.google.com/search?q=dog+images&rlz=1C1JZAP_enUS702US702&tbm=isch&tbo=u&source=univ&sa=X&sqi=2&ved=2ahUKEwiLp-a0-a_fAhVowYsKHeQkDyIQsAR6BAgBEAE&biw=1396&bih=657#imgrc=vOCatyKQLhYr0M",
+    name: "Kate",
+    photo : "assests/images/longhair.png",
     scores : [5,
       1,
       4,

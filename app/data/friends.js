76d81f7cd6a0eecd72ad5d98ee4dd93dcbba34e6
saranyamohandas <saranyamohandas2@gmail.com>
2018-12-20 19:12:42 -0800

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,37 @@
-var friendList = [{
+var friendList = [
+    {
     name: "Moksh",
     photo : "https://www.google.com/search?q=m+images&rlz=1C1JZAP_enUS702US702&tbm=isch&tbo=u&source=univ&sa=X&ved=2ahUKEwjQjdzv2K3fAhUL3lQKHU57C8sQsAR6BAgAEAE&biw=1536&bih=754&dpr=1.25#imgrc=GFcoUkc5jtT46M",
+    scores : [5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+             ]
+},
+    {
+    name: "Bo",
+    photo : "https://www.google.com/search?q=dog+images&client=firefox-b-1-ab&tbm=isch&tbo=u&source=univ&sa=X&ved=2ahUKEwiKtu39-K_fAhVDKH0KHTE7B5sQsAR6BAgDEAE&biw=1536&bih=750#imgrc=OjyG2p3XfwpfOM",
+    scores : [5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+             ]
+},
+    {
+    name: "Jack",
+    photo : "https://www.google.com/search?q=dog+images&rlz=1C1JZAP_enUS702US702&tbm=isch&tbo=u&source=univ&sa=X&sqi=2&ved=2ahUKEwiLp-a0-a_fAhVowYsKHeQkDyIQsAR6BAgBEAE&biw=1396&bih=657#imgrc=vOCatyKQLhYr0M",
     scores : [5,
       1,
       4,
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,11 +11,13 @@ module.exports = function(app){
     // req.body is available since we're using the body parsing middleware
     //if (tableData.length < 5) {
       console.log("in post")
-      findMatch(req.body);
+      var getMatch = findMatch(req.body);
+      console.log("getmatch",getMatch);
       friendData.push(req.body);
       
-      res.json(true);
-      //console.log(friendData);
+      //res.json(true);
+      res.json(getMatch);
+     
     //}
     
   })
@@ -23,19 +25,33 @@ module.exports = function(app){
 
 function findMatch(userInp){
     //var diff = [];
-    var diff = 0; // calculate total of difference b/w scores
+   // var diff = 0; // calculate total of difference b/w scores
     var matchCheck = [];
-    console.log("userinp - ",userInp)
-    console.log("friend data - ",friendData)
+    //console.log("userinp - ",userInp)
+    //console.log("friend data - ",friendData)
+     var rating = {};
     for(i=0;i<friendData.length;i++){
+        var diff = 0;
         for(j=0;j<friendData[i].scores.length;j++){
-            diff += Math.abs(friendData[i].scores[j]) - userInp.scores[j];
+            //console.log(friendData[i].scores[j],userInp.scores[j])
+            console.log(friendData[i].name);
+            console.log(userInp.name)
+            diff += Math.abs((friendData[i].scores[j]) - parseInt(userInp.scores[j]))
+            //console.log("diff",diff);
         }
         matchCheck.push(diff);
+        
+        
     }
-    matchCheck.sort(function(a,b){
-        return (a-b);
-    })
-    return(matchCheck[0])
+//    matchCheck.sort(function(a,b){
+//        return (a-b);
+//    });
+    console.log(matchCheck);
+    var getMin = Math.min(...matchCheck); //returns min arr
+    var getData = matchCheck.indexOf(getMin);
+//    console.log(getMin);
+//    console.log(getData);
+   return(friendData[getData]);
+   // return(matchCheck[0])
 }
 
